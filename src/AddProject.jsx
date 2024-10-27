58b0854157bbb8f5cc950f2d875eba96a282d85b
Artur <artur.czerwiak@gmail.com>
2024-10-27 17:42:11 +0100

    Create saving projects functionality

--- src/AddProject.jsx
+++ src/AddProject.jsx
@@ -1,13 +1,27 @@
-export default function AddProject() {
+import { useRef } from 'react';
+
+export default function AddProject({ onSave }) {
 	const labelClassName = 'uppercase font-bold text-lg  text-stone-500 mt-6';
 	const inputClassName =
 		'p-2 bg-stone-200 text-lg text-stone-600 rounded-sm focus:outline-none focus:border-b-4 focus:border-stone-500';
+	const title = useRef();
+	const description = useRef();
+	const date = useRef();
+	function handleSave() {
+		onSave(title.current.value, description.current.value, date.current.value);
+		title.current.value = '';
+		description.current.value = '';
+		date.current.value = '';
+	}
 
 	return (
 		<div className="">
 			<div className="flex flex-row justify-end gap-x-4 gap-y-2">
 				<button className="">Cancel</button>
-				<button className="bg-black rounded-md text-stone-200 py-3 px-6">
+				<button
+					onClick={handleSave}
+					className="bg-black rounded-md text-stone-200 py-3 px-6"
+				>
 					Save
 				</button>
 			</div>
@@ -16,16 +30,20 @@ export default function AddProject() {
 				<label htmlFor="title" className={labelClassName}>
 					Title
 				</label>
-				<input id="title" type="text" className={inputClassName} />
+				<input ref={title} id="title" type="text" className={inputClassName} />
 				<label htmlFor="description" className={labelClassName}>
 					Description
 				</label>
-				<textarea id="description" className={inputClassName}></textarea>
+				<textarea
+					ref={description}
+					id="description"
+					className={inputClassName}
+				></textarea>
 
 				<label htmlFor="date" className={labelClassName}>
 					Due date
 				</label>
-				<input type="date" id="date" className={inputClassName} />
+				<input ref={date} type="date" id="date" className={inputClassName} />
 			</form>
 		</div>
 	);
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import Sidebar from './Sidebar';
 import AddProject from './AddProject';
 function App() {
+	let yourProjects = [];
+
+	function handleSaveProject(title, description, date) {
+		yourProjects.push({
+			title: title,
+			description: description,
+			date: date,
+		});
+
+		console.log(yourProjects);
+	}
+
 	return (
 		<main className="flex flex-row justify-stretch h-dvh pt-10">
 			<Sidebar />
 			<section id="workspace" className="py-16 px-8 grow">
-				<AddProject />
+				<AddProject onSave={handleSaveProject} />
 			</section>
 		</main>
 	);
