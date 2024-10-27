a48220f9f04b00f0dec77126d06a5f2d384768b7
Artur <artur.czerwiak@gmail.com>
2024-10-27 20:23:31 +0100

    Add listing of created projects on sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import { useState } from 'react';
 import Sidebar from './Sidebar';
 import AddProject from './AddProject';
 function App() {
-	let yourProjects = [];
+	const [yourProjects, setYourProjects] = useState([]);
 	const [showAddProject, setShowAddProject] = useState(false);
-	function handleSaveProject(title, description, date) {
-		yourProjects.push({
-			title: title,
-			description: description,
-			date: date,
-		});
 
+	function handleSaveProject(title, description, date) {
+		setYourProjects((prevProjects) => {
+			const updatedYourProjects = [
+				{
+					title: title,
+					description: description,
+					date: date,
+				},
+				...prevProjects,
+			];
+
+			return updatedYourProjects;
+		});
+    
 		console.log(yourProjects);
 	}
 
@@ -24,7 +32,10 @@ function App() {
 
 	return (
 		<main className="flex flex-row justify-stretch h-dvh pt-10">
-			<Sidebar onClickAddButton={handleShowAddProject} />
+			<Sidebar
+				onClickAddButton={handleShowAddProject}
+				projects={yourProjects}
+			/>
 			<section id="workspace" className="py-16 px-8 grow">
 				{showAddProject && (
 					<AddProject
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,4 +1,4 @@
-export default function Sidebar({ onClickAddButton }) {
+export default function Sidebar({ onClickAddButton, projects }) {
 	return (
 		<aside className="w-1/4 bg-black text-white py-10 px-8 rounded-tr-2xl">
 			<h2 className="my-10 text-2xl font-bold uppercase">Your projects</h2>
@@ -8,6 +8,13 @@ export default function Sidebar({ onClickAddButton }) {
 			>
 				+ Add project
 			</button>
+			<ul className="mt-10">
+				{projects.map((e, index) => (
+					<li key={index} className="p-2 hover:bg-stone-800">
+						<button className="">{e.title}</button>
+					</li>
+				))}
+			</ul>
 		</aside>
 	);
 }
