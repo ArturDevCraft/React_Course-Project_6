688e91dc55c786ae6ed050bbb1af3ae849ac4cf0
Artur <artur.czerwiak@gmail.com>
2024-10-28 21:03:15 +0100

    Add css styles for EditProject component

--- src/components/EditProject.jsx
+++ src/components/EditProject.jsx
@@ -2,10 +2,13 @@ export default function EditProject({ title, description, date }) {
 	return (
 		<div>
 			<div className="flex justify-between">
-				<h2>{title}</h2> <button>Delete</button>
+				<h2 className="text-4xl font-bold text-stone-700">{title}</h2>{' '}
+				<button className="text-xl text-stone-700 hover:text-stone-500">
+					Delete
+				</button>
 			</div>
-			<p>{date}</p>
-			<p>{description}</p>
+			<p className="text-stone-400 text-lg mt-4">{date}</p>
+			<p className="text-lg text-stone-700 mt-4">{description}</p>
 		</div>
 	);
 }
