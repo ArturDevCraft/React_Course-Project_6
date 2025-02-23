21ba18bba0fa22c505d9ad4387caa5c24f49afb6
Artur <artur.czerwiak@gmail.com>
2025-02-23 14:48:04 +0100

    Code cleanup

--- src/components/EditProject.jsx
+++ src/components/EditProject.jsx
@@ -16,12 +16,7 @@ export default function EditProject({
 		<div>
 			<div className="flex justify-between">
 				<h2 className="text-4xl font-bold text-stone-700">{title}</h2>
-				<button
-					className="text-xl text-stone-700 hover:text-stone-500"
-					onClick={handleDeleteProject}
-				>
-					Delete
-				</button>
+				<button onClick={handleDeleteProject}>Delete</button>
 			</div>
 			<p className="text-stone-400 text-lg mt-4">{date}</p>
 			<p className="text-lg text-stone-700 mt-4 mb-6">{description}</p>
